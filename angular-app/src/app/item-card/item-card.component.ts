4dbb7df3aef2b2fb2fa37f883467012ed42c4fec
Mishin Dmitry <dima_mishin_95@mail.ru>
2021-03-05 19:44:11 +0300

    made task 2

---
+++ angular-app/src/app/item-card/item-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-card',
+  template: `
+    <p>
+      item-card works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ItemCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-app/src/app/tooltip-with-icon/tooltip-with-icon.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tooltip-with-icon',
+  template: `
+    <p>
+      tooltip-with-icon works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TooltipWithIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
